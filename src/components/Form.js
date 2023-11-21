1e0f2cf96d124f003e776e14c4431e1675695898
Murilo Lima Matioli <murilo.matioli@pmisa.net>
2023-11-21 16:40:11 -0300

    Modularização

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,7 +8,7 @@ function Form(){
         <>        
         <div className={styles.formWrapper}>
             <form className={styles.formulario} onSubmit={enviarDados}>
-                        <h2 className={styles.formTitle}>Criar formulário</h2>
+                <h2 className={styles.formTitle}>Criar formulário</h2>
                 <label for="nome" className={styles.labelStyle}>Nome: <input type="text" id="nome" placeholder="Digite o nome"></input></label>
                 <label for="email" className={styles.labelStyle}>Email: <input type="text" id="email" placeholder="Digite o email"></input></label>
                 <label for="endereco" className={styles.labelStyle}>Endereço: <input type="text" id="endereco" placeholder="Digite o endereço"></input></label>
@@ -16,9 +16,7 @@ function Form(){
                 <label for="lotacao" className={styles.labelStyle}>Lotacao: <input type="text" id="cpf" placeholder="Informe o setor"></input></label>
 
                 <SexoOptions></SexoOptions>
-                <label for="selecaoHabilitacao" className={styles.labelSelect}>Tem carteira de motorista?
-                    <HabilitacaoOptions></HabilitacaoOptions>
-                </label>
+                <HabilitacaoOptions></HabilitacaoOptions>
                 <label for="birthDate" className={styles.labelDate}>Data de nascimento:<input type='date' id="birthDate" className={styles.date}></input></label>
                 <input type='submit' value="Cadastrar"className={styles.submitForm}></input>
             </form>
--- src/components/HabilitacaoOptions.js
+++ src/components/HabilitacaoOptions.js
@@ -3,15 +3,17 @@ import styles from './Habilitacao.module.css';
 function HabilitacaoOptions() {
     return (
         <>
-        <select id="selecaoHabilitacao" className={styles.select}>
-            <option value="">N/A</option>
-            <option value="habA">A</option>
-            <option value="habB">B</option>
-            <option value="habAB">AB</option>
-            <option value="habC">C</option>
-            <option value="habD">D</option>
-            <option value="habE">E</option>
-        </select>
+        <label for="selecaoHabilitacao" className={styles.labelSelect}>Tem carteira de motorista?
+            <select id="selecaoHabilitacao" className={styles.select}>
+                <option value="">N/A</option>
+                <option value="habA">A</option>
+                <option value="habB">B</option>
+                <option value="habAB">AB</option>
+                <option value="habC">C</option>
+                <option value="habD">D</option>
+                <option value="habE">E</option>
+            </select>
+        </label>
         </>
     );
 }
